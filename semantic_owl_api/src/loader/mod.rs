53ca4715fe1314257709c0d366a71a4ddf39f67d
DavidDexter <dmwangi@kineticengines.co.ke>
2021-06-05 20:11:20 +0300

    feat : add custom parser
    - assemble document atoms to TurtleDocument struct
    
    Signed-off-by: DavidDexter <dmwangi@kineticengines.co.ke>

--- semantic_owl_api/src/loader/mod.rs
+++ semantic_owl_api/src/loader/mod.rs
@@ -12,20 +12,22 @@ pub fn load_document(path: &str) -> std::io::Result<()> {
   let file = File::open(path)?;
   let reader = BufReader::new(file);
 
-  let mut document = TurtleSignature::new();
+  let mut document = TurtleDocument::new();
   for (_, line) in reader.lines().enumerate() {
     let ln = line?;
     let result = parse_turtle(ln.as_str());
     if let Ok(result) = result {
       let (_, kind) = result;
-      // TODO: Assemble all into a data structure the represents the entire document
       match kind {
         StatementKind::Comment => {}
         StatementKind::BasePrefix => {
+          let header = TurtleHeaderItem::new(true, "", "", "");
+          document.headers.push_back(header)
+        }
+        StatementKind::NormPrefix => {
           let header = TurtleHeaderItem::new(false, "", "", "");
           document.headers.push_back(header)
         }
-        StatementKind::NormPrefix => {}
         StatementKind::PartOf => {}
         StatementKind::Whitespace => {}
         StatementKind::None => {}
