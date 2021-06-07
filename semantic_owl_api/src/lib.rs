bb2c7297859b8c37aba941c383fd1f2dea248fa3
DavidDexter <dmwangi@kineticengines.co.ke>
2021-06-07 21:47:20 +0300

    feat : add more parsers
    Signed-off-by: DavidDexter <dmwangi@kineticengines.co.ke>

--- semantic_owl_api/src/lib.rs
+++ semantic_owl_api/src/lib.rs
@@ -1,7 +1,7 @@
 //! Semantic Owl API
 //!
 //! This is a Rust implementation of owlapi which was originally written in Java.
-//! However, Semantic Owl Api is not a direct one-to-one implementation of owlapi.
+//! Semantic Owl Api is not a direct one-to-one implementation of owlapi.
 //! While ideas are borrowed, the implementation is not.
 
 mod declarations;
