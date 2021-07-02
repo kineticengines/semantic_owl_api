772f507e3104f6fc2df468c5ca26937e5ecad114
DavidDexter <dmwangi@kineticengines.co.ke>
2021-07-02 18:33:06 +0300

    feat : make document consume owned data
    Signed-off-by: DavidDexter <dmwangi@kineticengines.co.ke>

--- semantic_owl_api/src/declarations/turtle.rs
+++ semantic_owl_api/src/declarations/turtle.rs
@@ -1,18 +1,33 @@
 //! Turtle module defines representaion of turtle documents
+use serde::{Deserialize, Serialize};
 
 use std::{collections::VecDeque, iter::FromIterator};
 
 /// StatementKind used to map turtke parse results
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum StatementKind {
+  // e.g -> ###  http://www.ontologyrepository.com/CommonCoreOntologies/Bent
   Comment,
+
+  // e.g -> @base <http://www.ontologyrepository.com/CommonCoreOntologies/Mid/QualityOntology> .
   BasePrefix,
+
+  // e.g -> @prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .
   NormPrefix,
+
+  // e.g -> cco:is_curated_in_ontology "http://www.ontologyrepository.com/CommonCoreOntologies/Mid/QualityOntology"^^xsd:anyURI ;
   PartOf,
+
+  // e.g -> rdfs:label "Vermilion"@en .
   StatementWithTerminator,
+
   Whitespace,
+
+  // e.g -> .
   Terminator,
+
   NotATurtle,
+
   None,
 }
 
@@ -32,29 +47,29 @@ pub enum StatementKind {
 /// ```ttl
 /// @base <http://example.org/> .
 /// ```
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub struct TurtleHeaderItem<'document, R> {
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct TurtleHeaderItem {
   // determines whether the header item is a `base` or not
   pub is_base: bool,
 
   // the prefix namespace. Example; skos, owl, rdfs, xsd,umls.
   // this will be absent for base header items
-  pub prefix_namespace: &'document str,
+  pub prefix_namespace: Option<String>,
 
   // the URL where the prefix points to. Example <http://www.w3.org/2004/02/skos/core#>
-  pub prefix_iri: &'document str,
+  pub prefix_iri: Option<String>,
 
   // the raw item string
-  pub raw_header: R,
+  pub raw_header: Option<String>,
 }
 
-impl<'document, R> TurtleHeaderItem<'document, R> {
+impl TurtleHeaderItem {
   pub fn new(
     is_base: bool,
-    prefix_namespace: &'document str,
-    prefix_iri: &'document str,
-    raw_header: R,
-  ) -> TurtleHeaderItem<'document, R> {
+    prefix_namespace: Option<String>,
+    prefix_iri: Option<String>,
+    raw_header: Option<String>,
+  ) -> TurtleHeaderItem {
     Self {
       is_base,
       prefix_namespace,
@@ -75,28 +90,28 @@ impl<'document, R> TurtleHeaderItem<'document, R> {
 ///     owl:versionInfo "2020ab" .
 ///```
 ///
-#[derive(Debug, Clone, PartialEq)]
-pub struct TurtleBodyItem<'document> {
-  pub subject: &'document str,
-  pub predicate_object: VecDeque<TurtlePredicateObject<'document>>,
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct TurtleBodyItem {
+  pub subject: Option<String>,
+  pub predicate_object: VecDeque<TurtlePredicateObject>,
 }
 
 /// TurtlePredicateObject is a combination of predicate and object retrieved
 /// from a turtle statement.
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub struct TurtlePredicateObject<'document> {
-  pub raw_predicate_object: &'document str,
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct TurtlePredicateObject {
+  pub raw_predicate_object: Option<String>,
 
   // represents the predicate part of a turtle triple
   // predicate can be prefixed or be an URI
-  pub predicate: &'document str,
+  pub predicate: Option<String>,
 
   // indicates whether the predicate is a URI or not
   pub predicate_is_url: bool,
 
   // represents the object part of a turtle triple
   // object can be prefixed or be an URI
-  pub object: &'document str,
+  pub object: Option<String>,
 
   // indicates whether the object is a URI or not
   pub object_is_url: bool,
@@ -105,49 +120,52 @@ pub struct TurtlePredicateObject<'document> {
 /// TurtleDocument is the composition of an entire turtle document. It is the sum of turle headers and body items.
 /// A turtle document can be very large. This struct is used to represent such a document
 /// as a summation of it'document atomic structures
-#[derive(Debug, Clone, PartialEq, Default)]
-pub struct TurtleDocument<'document, R> {
-  pub headers: VecDeque<TurtleHeaderItem<'document, R>>,
-  pub body: VecDeque<TurtleBodyItem<'document>>,
+#[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize)]
+pub struct TurtleDocument {
+  pub headers: VecDeque<TurtleHeaderItem>,
+  pub body: VecDeque<TurtleBodyItem>,
 }
 
-impl<'document, R> TurtleDocument<'document, R>
-where
-  R: Copy,
-{
-  pub fn new() -> TurtleDocument<'document, R> {
-    let headers: VecDeque<TurtleHeaderItem<'document, R>> = VecDeque::new();
-    let body: VecDeque<TurtleBodyItem<'document>> = VecDeque::new();
-    TurtleDocument { headers, body }
+impl TurtleDocument {
+  pub fn new() -> TurtleDocument {
+    let headers: VecDeque<TurtleHeaderItem> = VecDeque::new();
+    let body: VecDeque<TurtleBodyItem> = VecDeque::new();
+    Self { headers, body }
   }
 
   /// base_iri returns the IRI of the base prefix
   /// example
   /// `@base <http://example.org/> .` returns Option of `<http://example.org/>`
-  fn base_iri(&'document self) -> Option<&'document str> {
-    let base: VecDeque<TurtleHeaderItem<'document, R>> =
-      self.headers.iter().filter(|x| x.is_base).collect();
+  pub fn base_iri<'a>(&'a self) -> Option<String> {
+    let base: VecDeque<TurtleHeaderItem> = self.headers.iter().filter(|x| x.is_base).collect();
     match base.len() {
       1 => {
-        let raw = base[0].prefix_iri;
-        let raw = raw.strip_prefix('<')?;
-        let raw = raw.strip_suffix('>')?;
-        Some(raw)
+        let h = base[0].clone();
+        let raw = h.prefix_iri;
+        let b = {
+          match raw {
+            Some(x) => {
+              let r = &x;
+              let r = r.strip_prefix('<')?;
+              let r = r.strip_suffix('>')?;
+              Some(String::from(r))
+            }
+            None => None,
+          }
+        };
+        b
       }
       _ => None,
     }
   }
 }
 
-impl<'document, R: 'document> FromIterator<&'document TurtleHeaderItem<'document, R>>
-  for VecDeque<TurtleHeaderItem<'document, R>>
-where
-  R: Copy,
-{
-  fn from_iter<T: IntoIterator<Item = &'document TurtleHeaderItem<'document, R>>>(iter: T) -> Self {
-    let mut headers: VecDeque<TurtleHeaderItem<'document, R>> = VecDeque::new();
+impl<'a> FromIterator<&'a TurtleHeaderItem> for VecDeque<TurtleHeaderItem> {
+  fn from_iter<T: IntoIterator<Item = &'a TurtleHeaderItem>>(iter: T) -> Self {
+    let mut headers: VecDeque<TurtleHeaderItem> = VecDeque::new();
     for h in iter {
-      headers.push_back(*h);
+      let i = h.clone();
+      headers.push_back(i);
     }
     headers
   }
@@ -162,38 +180,24 @@ mod tests {
     let mut document = TurtleDocument::new();
     let header = TurtleHeaderItem::new(
       true,
-      "@base",
-      "<http://example.org/>",
-      "@base <http://example.org/> .",
+      Some(String::from("@base")),
+      Some(String::from("<http://example.org/>")),
+      Some(String::from("@base <http://example.org/> .")),
     );
     document.headers.push_back(header);
     let iri0 = document.base_iri();
     let iri1 = document.base_iri();
-    assert_eq!(Some(iri0), Some(iri1));
+    assert_eq!(iri0, iri1);
   }
 
   #[test]
   fn should_return_base_prefix1() {
     let mut document = TurtleDocument::new();
     let header = TurtleHeaderItem::new(
-      true,
-      "@base",
-      "http://example.org/",
-      "@base <http://example.org/> .",
-    );
-    document.headers.push_back(header);
-    let iri0 = document.base_iri();
-    assert_eq!(iri0, None);
-  }
-
-  #[test]
-  fn should_return_base_prefix2() {
-    let mut document = TurtleDocument::new();
-    let header = TurtleHeaderItem::new(
       false,
-      "@base",
-      "<http://example.org/>",
-      "@base <http://example.org/> .",
+      Some(String::from("@base")),
+      Some(String::from("<http://example.org/>")),
+      Some(String::from("@base <http://example.org/> .")),
     );
     document.headers.push_back(header);
     let iri0 = document.base_iri();
